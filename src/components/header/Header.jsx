d464e0280f0b387b7e81a448235a8ad2df32b32b
khe4oyan <khe4oyan@bk.ru>
2023-06-20 23:26:51 +0400

    todo's save in local storage

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,24 +11,24 @@ export default function Header({setTodo}) {
 	}
 
 	useEffect(() => {
-    function handleKeyPress(event) {
+    const handleKeyPress = (event) => {
       if (event.key === 'Enter') {
 				change(value);
 				setValue('');
       }
     }
-		
     window.addEventListener('keydown', handleKeyPress);
-		
     return () => {
 			window.removeEventListener('keydown', handleKeyPress);
     };
   }, [value]);
 
 	return (
-		<header>
-			<input onChange={(e) => {setValue(e.target.value); }} type="text" placeholder="Add a task.." value={value}/>
-			<button onClick={() => {change(value); setValue('')}}>add</button>
+		<header className='center container'>
+			<div className='header__box'>
+				<input onChange={(e) => {setValue(e.target.value); }} type="text" placeholder="Add a task.." value={value}/>
+				<button onClick={() => {change(value); setValue('')}}>add</button>
+			</div>
 		</header>
 	);
 }
